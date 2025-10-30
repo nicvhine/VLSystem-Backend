65bf0e5ac87f90495b8961e5929d7c43b830ddd3
nicvhine <99799695+nicvhine@users.noreply.github.com>
2025-10-30 08:56:32 +0800

    Added phone number field for borrowers_account

--- schemas/borrowerSchema.js
+++ schemas/borrowerSchema.js
@@ -1,15 +1,16 @@
 const { z } = require('zod');
 
 const borrowerSchema = z.object({
-    borrowersId: z.string(),
-    name: z.string(),
-    role: z.string(), 
-    username: z.string(),
-    password: z.string(), 
-    isFirstLogin: z.boolean().default(true),
-    assignedCollector: z.string(),
-    email: z.string().email(),
-    profilePic: z.string().url().optional()
+  borrowersId: z.string(),
+  name: z.string(),
+  role: z.string(),
+  username: z.string(),
+  password: z.string(),
+  isFirstLogin: z.boolean().default(true),
+  assignedCollector: z.string(),
+  email: z.string().email(),
+  phoneNumber: z.string().optional(), 
+  profilePic: z.string().url().optional(),
 });
 
 module.exports = borrowerSchema;
