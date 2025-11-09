34ec89fb74b50c57c92b76d3a03317ca92a8479b
nicvhine <99799695+nicvhine@users.noreply.github.com>
2025-11-09 11:49:12 +0800

    Fix imports

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 3001,
     MONGODB_URI: process.env.MONGODB_URI,
     CORS_OPTIONS: {
-        origin: 'http://localhost:3000',
+        origin: ['http://localhost:3000', 'https://vlsystem.vistula.ph'],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
