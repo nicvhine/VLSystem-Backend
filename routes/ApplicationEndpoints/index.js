64c0d5c2321db8de0203c99c301e16a14471708d
Creszra <josephrmagabilin@gmail.com>
2025-11-24 18:43:28 +0800

    fix uploading and deleting documents in loan application page

--- routes/ApplicationEndpoints/index.js
+++ routes/ApplicationEndpoints/index.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const postApplication = require("./postApplication");
 const getApplication = require("./getApplication");
 const putApplication = require("./putApplication");
+const uploadDocuments = require("./uploadDocuments");
 
-// Mount application endpoints (create, read, update)
+// Mount application endpoints (create, read, update, upload)
 module.exports = (db) => {
   router.use("/", postApplication(db));
   router.use("/", getApplication(db));
   router.use("/", putApplication(db));
+  router.use("/", uploadDocuments(db));
   return router;
 };
